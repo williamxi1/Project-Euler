1db614a8e1db29fab9f8900520a49b082afa534d
William Xi <williamxiisthecoolest@gmail.com>
2019-07-20 17:58:25 -0400

    Added unsaved problems

---
+++ euler69.py
@@ -0,0 +1,9 @@
+# Euler's Totient function, φ(n) [sometimes called the phi function],
+# is used to determine the number of numbers less than n which are relatively prime to n.
+# For example, as 1, 2, 4, 5, 7, and 8, are all less than nine and relatively prime to nine, φ(9)=6.
+#
+# Find the value of n ≤ 1,000,000 for which n/φ(n) is a maximum.
+
+answer = 2*3*5*7*11*13*17
+
+print(answer)
